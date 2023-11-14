752f6d812726860dd07e3203ef9e55278733698c
Rahulraj <rahulrajkattoor@gmail.com>
2023-11-14 09:37:45 +0530

    commit

---
+++ bin/rahul.dart
@@ -0,0 +1,5 @@
+import 'package:rahul/rahul.dart' as rahul;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rahul.calculate()}!');
+}
---
+++ bin/sample.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main(){
+  print("enter 2 number");
+  var num=1 std.in 
+
+
+
+}
