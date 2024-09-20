c0da0dfb92fcbc0fb14b263f2c06a53a3fc8233d
Rahulraj <rahulrajkattoor@gmail.com>
2024-09-20 19:01:41 +0530

    commit

---
+++ bin/Swaping number without third variable/Swapping.dart
@@ -0,0 +1,14 @@
+void main(){
+  // int a=5;
+  // int b=10;
+  // a=a+b;
+  // b=a-b;
+  // a=a-b;
+  // print("a:$a,b:$b");
+  int a=5;
+  int b=10;
+  a=a^b;
+  b=a^b;
+  a=a^b;
+  print("a:$a,b:$b");
+}
